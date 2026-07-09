6fb9551b40b2a7941a780458a756cfb56df20887
atharvaarbat <arbatatharva130@gmail.com>
2026-07-10 02:02:33 +0530

    chore: scaffold Vite + React + TS app, add Tailwind + shadcn/ui, remove placeholder.html

---
+++ challenges/challenge-b/src/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Button} from './components/ui/button'
+import { Card, CardContent } from './components/ui/card'
+
+function App() {
+
+  return (
+    <>
+    <Card>
+      <CardContent>
+
+      <Button>
+Hello
+      </Button>
+      </CardContent>
+    </Card>
+
+    </>
+  )
+}
+
+export default App
